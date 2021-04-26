ff167d129ea6ffd548460f32095417b5955d45d5
Guy Broadhurst <guy@guybroadhurst.co.uk>
2021-04-26 08:31:30 +0100

    Finalised version

---
+++ main.js
@@ -0,0 +1,24 @@
+$('.show-filters').on('click', function() {
+  if ($('.filters-list').is(':visible')) {
+    $('.filters-list').slideUp()
+    $('.show-filters').text('Show Filters')
+  } else {
+    $('.filters-list').slideDown()
+    $('.show-filters').text('Hide Filters')
+  }
+  
+  return false
+})
+
+$('.filters-list a').on('click', function() {
+  
+  var filter = $(this).attr('data-filter')
+  
+	$('.product').hide()
+  $(filter).show()
+  
+  $('.filters-list a').removeClass('selected')
+  $(this).addClass('selected')
+  
+  return false
+})
